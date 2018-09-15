a6ba8df67cda833c89db7e6445a88c96b066bed3
Baris Kurtlutepe <barisk@gmail.com>
2018-09-15 14:05:38 +0200

    removed crc8 related stuff from parsing

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@
 import sys
 import datetime
 import binascii
-from crc import crc8, crc16
+from crc import crc16
 
 
 lastSequences = [-1] * 8
@@ -48,23 +48,15 @@ def decode(data, timestamp):
         p_seq2 = (b9 & 0x3C) >> 2
         if (p_bodylen > 23):
             if p_bodylen == 24:
-                p_crc8 = ord(data[35])
-                c_crc8 = crc8(data[0:35])
                 p_body = binascii.hexlify(data[11:35])
                 remainingData = 0
             else:
-                p_crc8 = ord(data[36])
-                c_crc8 = crc8(data[0:36])
                 p_body = binascii.hexlify(data[11:36])
                 remainingData = p_bodylen - 25
-            crcOK = p_crc8 == c_crc8
         else:
-            p_crc8 = ord(data[13+p_bodylen])
-            c_crc8 = crc8(data[0:13+p_bodylen])
             p_crc16 = ord(data[11+p_bodylen]) << 8 | ord(data[12+p_bodylen])
             c_crc16 = crc16(data[5:11+p_bodylen])
-            crcOK = p_crc8 == c_crc8 and p_crc16 == c_crc16
-
+            crcOK = p_crc16 == c_crc16
         if (crcOK):
             p_body = binascii.hexlify(data[11:11+p_bodylen])
 
@@ -72,9 +64,6 @@ def decode(data, timestamp):
         p_addr2 = binascii.hexlify(data[5:9])
         p_bodylen = 0
         p_body = ""
-        p_crc8 = ord(data[9])
-        c_crc8 = crc8(data[0:9])
-        crcOK = p_crc8 == c_crc8
     elif p_type == "CON":
         p_addr2 = ""
         if remainingData > 31:
@@ -87,17 +76,10 @@ def decode(data, timestamp):
 
         remainingData -= p_bodylen
         p_body = binascii.hexlify(data[6:6+p_bodylen])
-        p_crc8 = ord(data[7+p_bodylen])
-        c_crc8 = crc8(data[0:7+p_bodylen])
-        crcOK = p_crc8 == c_crc8
     else:
         p_body = binascii.hexlify(data)
         p_bodylen = len(data)
         print(p_addr1, p_type, "0x%02x" % p_seq,  p_body)
-        for i in range(36, 5, -1):
-            if crc8(data[0:i]) == ord(data[i]):
-                print "possible crc at #", i
-                break
 
     if crcOK:
         if lastSequences[p_t] == p_seq:
