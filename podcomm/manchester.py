6604ac038108ecbe406da27a31b509178384b799
Baris Kurtlutepe <barisk@gmail.com>
2018-09-23 13:45:26 +0200

    added encoding support

--- podcomm/manchester.py
+++ podcomm/manchester.py
@@ -1,9 +1,12 @@
+import random
+
 class ManchesterCodec:
 
     def __init__(self):
         self.initializeLookupTable()
+        self.generateNonManchesterNoise()
 
-    def Decode(self, data):
+    def decode(self, data):
         decoded = ""
         for i in range(0, len(data), 2):
             word = data[i:i+2]
@@ -13,16 +16,22 @@ class ManchesterCodec:
                 break
         return decoded
     
-    def Encode(self, data):
-        pass
+    def encode(self, data):
+        encoded = ""
+        for i in data:
+            encoded += lookupdict[i]
+        encoded += self.noiseLines[self.noiseSeq]
+        self.noiseSeq += 1
+        self.noiseSeq %= 32
+        return encoded[:80]
 
     def initializeLookupTable(self):
         self.lookupdict = dict()
         for i in range(0, 256):
-            enc = self.manchesterEncodeSingleByte(i)
+            enc = self.encodeSingleByte(i)
             self.lookupdict[enc] = chr(i)
-
-    def manchesterEncodeSingleByte(self, d):
+        
+    def encodeSingleByte(self, d):
         e = 0
         for b in range (0,15, 2):
             if d & 0x01 == 0:
@@ -31,3 +40,13 @@ class ManchesterCodec:
                 e |= (1 << b)
             d = d >> 1
         return chr(e >> 8) + chr(e & 0xff)
+
+    def generateNonManchesterNoise():
+        self.noiseSeq = 0
+        noiseNibbles = '0123478bcdef'
+        self.noiseLines = []
+        for x in range(0, 32):
+            noiseLine = "f"
+            for i in range(0, 79):
+                noiseLine += random.choice(noiseNibbles)
+            self.noiseLines.apppend(noiseLine.decode("hex"))
