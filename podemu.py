0b487e503fdacf2b6f63a6a6cdb3c877ffd93c5c
Baris Kurtlutepe <barisk@gmail.com>
2018-09-26 00:04:20 +0200

    basic pod emulator - wip

--- podemu.py
+++ podemu.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python
+from podcomm.pod import Pod
+import binascii
 
 def main():
-    pass
+    pod = Pod(msgHandler, errHandler, 123456, 123456)
+    pod.start()
+    raw_input()
+    pod.stop()
+
+def msgHandler(msg):
+    for ctype, content in msg.getContents():
+        if ctype == 0x07:
+             address = binascii.hexlify(content)
+             print ("Assign pod address request, address: %s" % address)
+        else:
+            print ("Unknown message content, type: 0x%02X content: %s" % ctype, binascii.hexlify(content))
+
+
+
+def errHandler():
+    print "error"
 
 if __name__== "__main__":
   main()
