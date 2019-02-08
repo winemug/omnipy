6a46dbf124d1648ffcf89c0b7d93a61d02efe357
Barış Kurtlutepe <barisk@gmail.com>
2019-02-08 12:35:13 +0100

    bolus and very basic exception handling

--- restapi.py
+++ restapi.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+import sys
+import simplejson as json
 from flask import Flask, request
 from podcomm.pdm import Pdm
 from podcomm.pod import Pod
 from podcomm.packet import Packet
 from podcomm.rileylink import RileyLink
 from podcomm.crc import crc8
-import simplejson as json
+from Decimal import *
 
 app = Flask(__name__)
 
@@ -22,35 +24,50 @@ def respond_error(msg = "Unknown"):
 
 @app.route("/pdm/status")
 def get_status():
-    pdm = get_pdm()
-    pdm.updatePodStatus()
-    return respond(True, pdm.pod.__dict__)
+    try:
+        pdm = get_pdm()
+        pdm.updatePodStatus()
+        return respond(True, pdm.pod.__dict__)
+    except:
+        return respond_error(msg = sys.exc_info()[0])
 
 @app.route("/pdm/newpod")
 def grab_pod():
-    pod = Pod()
-    pod.lot = request.args.get('lot')
-    pod.tid = request.args.get('tid')
+    try:
+        pod = Pod()
+        pod.lot = request.args.get('lot')
+        pod.tid = request.args.get('tid')
 
-    r = RileyLink()
-    r.connect()
-    r.init_radio()
-    p = None
-    while True:
-        data = r.get_packet(30000)
-        if data is not None and len(data) > 2:
-            calc = crc8(data[2:-1])
-            if data[-1] == calc:
-                p = Packet(0, data[2:-1])
-                break
-    r.disconnect()
+        r = RileyLink()
+        r.connect()
+        r.init_radio()
+        p = None
+        while True:
+            data = r.get_packet(30000)
+            if data is not None and len(data) > 2:
+                calc = crc8(data[2:-1])
+                if data[-1] == calc:
+                    p = Packet(0, data[2:-1])
+                    break
+        r.disconnect()
 
-    if p is None:
-        respond_error("No pdm packet detected")
+        if p is None:
+            respond_error("No pdm packet detected")
 
-    pod.address = p.address
-    pod.Save("pod.json")
-    return respond_ok({"address": p.address})
+        pod.address = p.address
+        pod.Save("pod.json")
+        return respond_ok({"address": p.address})
+    except:
+        return respond_error(msg = sys.exc_info()[0])
+
+@app.route("/pdm/bolus")
+def bolus():
+    try:
+        amount = Decimal(request.args.get('amount'))
+        self.pdm.bolus(amount, False)
+        return respond(True, pdm.pod.__dict__)
+    except:
+        return respond_error(msg = sys.exc_info()[0])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4444)
