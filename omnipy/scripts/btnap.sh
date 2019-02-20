0419cd800f7b71f74fedbe4cd459a29c9c8b7abd
Barış Kurtlutepe <barisk@gmail.com>
2019-02-20 16:29:35 +0100

    changed bt service to run as root

--- omnipy/scripts/btnap.sh
+++ omnipy/scripts/btnap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo killall bt-network > /dev/null 2>&1
+killall bt-network > /dev/null 2>&1
 while true
 do
-    sudo bt-network -c $1 nap > /dev/null 2>&1
+    bt-network -c $1 nap > /dev/null 2>&1
     sleep 5
 done
