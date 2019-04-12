9781d68cea9354e7677066ef96bcb92ae69bd8bb
Baris Kurtlutepe <barisk@gmail.com>
2019-04-12 20:05:14 +0200

    added bt setup as separate script

---
+++ scripts/bt-setup.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+sudo setcap 'cap_net_raw,cap_net_admin+eip' `which hciconfig`
+sudo setcap 'cap_net_raw,cap_net_admin+eip' `which hcitool`
+sudo setcap 'cap_net_raw,cap_net_admin+eip' `which btmgmt`
+sudo setcap 'cap_net_raw,cap_net_admin+eip' `which bt-agent`
+sudo setcap 'cap_net_raw,cap_net_admin+eip' `which bt-network`
+sudo setcap 'cap_net_raw,cap_net_admin+eip' `which bt-device`
+sudo find / -name bluepy-helper -exec setcap 'cap_net_raw,cap_net_admin+eip' {} \;
+
+sudo systemctl disable omnipy-pan.service
+
+echo
+echo "Removing existing bluetooth devices"
+sudo btmgmt power on
+sudo bt-device -l | grep -e \(.*\) --color=never -o| cut -d'(' -f2 | cut -d')' -f1 | while read -r mac
+do
+        if [ !mac ]; then
+                sudo bt-device -d $mac
+                sudo bt-device -r $mac
+        fi
+done
+echo
+echo "Activating bluetooth pairing mode"
+sudo btmgmt connectable yes
+sudo btmgmt discov yes
+sudo btmgmt pairable yes
+sudo killall bt-agent
+sudo bt-agent -c NoInputNoOutput -d
+echo "Bluetooth device is now discoverable"
+echo
+echo "Open ${bold}bluetooth settings${normal} on your phone to search for and ${bold}pair${normal} with this device"
+echo "If you have already paired it on your phone, please unpair it first, then pair again"
+echo
+printf "Waiting for connection.."
+
+btdevice=
+while [[ -z "$btdevice" ]]
+do
+        printf "."
+        sleep 1
+        btdevice=`sudo bt-device -l | grep -e \(.*\)`
+done
+
+sudo btmgmt discov no
+
+echo
+
+echo "${bold}Paired with $btdevice.${normal}"
+mac=`echo $btdevice | cut -d'(' -f2 | cut -d')' -f1`
+
+echo
+echo
+echo "Please ${bold}enable bluetooth tethering${normal} on your phone if it's not already enabled"
+echo "Waiting for connection."
+echo "addr=$mac" > /home/pi/omnipy/scripts/btnap-custom.sh
+cat /home/pi/omnipy/scripts/btnap.sh >> /home/pi/omnipy/scripts/btnap-custom.sh
+sudo cp /home/pi/omnipy/scripts/omnipy-pan.service /etc/systemd/system/
+sudo systemctl enable omnipy-pan.service
+sudo systemctl start omnipy-pan.service
+ipaddr=
+while [[ -z "$ipaddr" ]]
+do
+        printf "."
+        sleep 1
+        ipaddr=`sudo ip -o -4 address | grep bnep0 | grep -e inet.*/ -o | cut -d' ' -f2 | cut -d'/' -f1`
+done
+echo
+echo
+echo "${bold}Connection test succeeeded${normal}. IP address: $ipaddr"
