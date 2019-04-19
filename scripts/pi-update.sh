6eff9b0f1e6bfaf24087f8c80f9a4a0e28ed83c3
Baris Kurtlutepe <barisk@gmail.com>
2019-04-19 23:29:28 +0200

    perform git pull before stopping services

--- scripts/pi-update.sh
+++ scripts/pi-update.sh
@@ -4,10 +4,17 @@ if [[ ! -d /home/pi/omnipy ]]
 then
 
 echo
-echo "You don't seem to have omnipy installed, please run the pi-setup.sh script first"
+echo "Omnipy is not installed"
 exit
 
 fi
+
+echo "Updating omnipy"
+cd /home/pi/omnipy
+git config --global user.email "omnipy@balya.net"
+git config --global user.name "Omnipy Setup"
+git stash
+git pull
 
 echo
 echo "Stopping omnipy services"
@@ -18,12 +25,5 @@ sudo systemctl disable omnipy.service
 sudo systemctl disable omnipy-beacon.service
 sudo systemctl disable omnipy-pan.service
 
-
-echo "Updating omnipy"
-cd /home/pi/omnipy
-git config --global user.email "omnipy@balya.net"
-git config --global user.name "Omnipy Setup"
-git stash
-git pull
 
 /bin/bash /home/pi/omnipy/scripts/pi-update-finalize.sh
