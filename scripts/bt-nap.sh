db50a152d46885109d808225fa6030ca2e0737ab
Baris Kurtlutepe <barisk@gmail.com>
2019-04-22 18:11:59 +0200

    version update

--- scripts/bt-nap.sh
+++ scripts/bt-nap.sh
@@ -1,33 +1,37 @@
 #!/usr/bin/env bash
 
 pair_bt_device() {
+    echo "Enabling bluetooth discovery"
     sudo btmgmt power off
     sudo btmgmt ssp on
     sudo btmgmt connectable on
     sudo btmgmt pairable on
     sudo btmgmt discov on
     sudo btmgmt power on
-    bt_device=
-    while [[ -z "${bt_device}" ]]
-    do
-            sleep 1
-            bt_device=`sudo bt-device -l | grep -e \(.*\)`
+    echo "Waiting for a connection"
+    while : ; do
+        sleep 2
+        sudo bt-device -l > /dev/null 2>&1
+        [[ ! $? -eq 0 ]] || break
     done
 
+    echo "Pairing completed"
     sudo btmgmt discov off
-    echo "Paired with ${bt_device}"
+    sleep 15
 }
 
 while true;
 do
-    killall -9 bt-network > /dev/null 2>&1
     sudo btmgmt power off
     sudo btmgmt power on
     paired_devices=`bt-device -l | grep -e \(.*\) --color=never -o| cut -d'(' -f2 | cut -d')' -f1`
-    if [[ ! -z ${paired_devices} ]]; then
+    if [[ ! -z "${paired_devices}" ]]; then
         while read -r mac_address;
         do
-            /usr/bin/bt-network -c ${mac_address} nap > /dev/null 2>&1
+            echo "Connecting to ${mac_address}"
+            killall -9 bt-network > /dev/null 2>&1
+            /usr/bin/bt-network -c ${mac_address} nap
+            echo "Disconnected from ${mac_address} (or connection failed)"
         done <<< "${paired_devices}"
         sleep 15
     else
