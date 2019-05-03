b61013cc94caa7e8b43d74683a6a350c2c447a99
Baris Kurtlutepe <barisk@gmail.com>
2019-05-03 14:34:45 +0200

    remove time adjustment, that could lead to incorrect temp basal status interpretation

--- restapi.py
+++ restapi.py
@@ -215,25 +215,25 @@ def _api_result(result_lambda, generic_err_message):
         if g_deny:
             raise RestApiException("Pdm is shutting down")
 
-        if request.args.get('req_t') is not None:
-            req_time = int(request.args.get('req_t'))
-            local_time = int(time.time() * 1000)
-            difference_ms = (req_time - local_time)
-            if g_oldest_diff is None:
-                g_oldest_diff = local_time
-
-            if g_oldest_diff - local_time > 300:
-                g_time_diffs = [difference_ms]
-                g_oldest_diff = local_time
-            else:
-                g_time_diffs.append(difference_ms)
-
-            if len(g_time_diffs) > 3:
-                diff_avg = sum(g_time_diffs) / len(g_time_diffs)
-                g_time_diffs = []
-
-                if diff_avg > 30000 or diff_avg < -30000:
-                    _adjust_time(diff_avg)
+        # if request.args.get('req_t') is not None:
+        #     req_time = int(request.args.get('req_t'))
+        #     local_time = int(time.time() * 1000)
+        #     difference_ms = (req_time - local_time)
+        #     if g_oldest_diff is None:
+        #         g_oldest_diff = local_time
+        #
+        #     if g_oldest_diff - local_time > 300:
+        #         g_time_diffs = [difference_ms]
+        #         g_oldest_diff = local_time
+        #     else:
+        #         g_time_diffs.append(difference_ms)
+        #
+        #     if len(g_time_diffs) > 3:
+        #         diff_avg = sum(g_time_diffs) / len(g_time_diffs)
+        #         g_time_diffs = []
+        #
+        #         if diff_avg > 30000 or diff_avg < -30000:
+        #             _adjust_time(diff_avg)
 
         return _create_response(True,
                                response=result_lambda(), pod_status=_get_pod())
