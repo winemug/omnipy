0345f6b8bf148cda951a79bc43097cd3835cf8f4
Barış Kurtlutepe <barisk@gmail.com>
2019-05-12 02:05:31 +0200

    v1.4.3 build script

--- scripts/pi-update-finalize.sh
+++ scripts/pi-update-finalize.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 echo
 echo Updating service scripts and restarting services
+# remnants of hotspot
+sudo apt remove hostapd dnsmasq -y > /dev/null 2>&1
 sudo apt update && sudo apt upgrade -y
 
 echo Ensure installation of packages needed in v1.4.1
-sudo apt install -y python3-rpi.gpio hostapd dnsmasq
-sudo systemctl disable hostapd
-# sudo systemctl unmask hostapd
-sudo systemctl disable dnsmasq
-# sudo systemctl enable hostapd
-# sudo systemctl enable dnsmasq
-# sudo systemctl start hostapd
-# sudo systemctl stop hostapd
-# sudo systemctl stop dnsmasq
-sudo cp /home/pi/omnipy/scripts/image/default.dnsmasq /etc/default/dnsmasq
-sudo cp /home/pi/omnipy/scripts/image/default.hostapd /etc/default/hostapd
-sudo cp /home/pi/omnipy/scripts/image/hostapd.conf /etc/hostapd/
-sudo cp /home/pi/omnipy/scripts/image/dnsmasq.conf /etc/dnsmasq.d/
-sudo cp /home/pi/omnipy/scripts/image/dhcpcd.conf /etc/
+sudo apt install -y python3-rpi.gpio
+
+# remnants of hotspot
+sudo systemctl stop omnipy-hotspot.service > /dev/null 2>&1
+sudo systemctl disable omnipy-hotspot.service > /dev/null 2>&1
+sudo rm /etc/systemd/system/omnipy-hotspot.service > /dev/null 2>&1
+
+# sudo cp /home/pi/omnipy/scripts/image/default.dnsmasq /etc/default/dnsmasq
+# sudo cp /home/pi/omnipy/scripts/image/default.hostapd /etc/default/hostapd
+# sudo cp /home/pi/omnipy/scripts/image/hostapd.conf /etc/hostapd/
+# sudo cp /home/pi/omnipy/scripts/image/dnsmasq.conf /etc/dnsmasq.d/
+# sudo cp /home/pi/omnipy/scripts/image/dhcpcd.conf /etc/
 sudo cp /home/pi/omnipy/scripts/image/rc.local /etc/
 
 
@@ -34,18 +34,13 @@ sudo chown -R pi.pi /home/pi
 sudo cp /home/pi/omnipy/scripts/omnipy-pan.service /etc/systemd/system/
 sudo cp /home/pi/omnipy/scripts/omnipy.service /etc/systemd/system/
 sudo cp /home/pi/omnipy/scripts/omnipy-beacon.service /etc/systemd/system/
-sudo cp /home/pi/omnipy/scripts/omnipy-hotspot.service /etc/systemd/system/
-sudo rm /etc/systemd/system/omnipy-hotspot.service
 
 sudo systemctl enable omnipy.service
 sudo systemctl enable omnipy-beacon.service
 sudo systemctl enable omnipy-pan.service
-# sudo systemctl disable omnipy-hotspot.service
 sudo systemctl start omnipy.service
 sudo systemctl start omnipy-beacon.service
 sudo systemctl start omnipy-pan.service
-# sudo systemctl start omnipy-hotspot.service
-sudo systemctl daemon-reload
 
 echo
 echo Configuration updated.
