80be4b53ac80bafe48714f7ec9f6016c88f5e657
Barış Kurtlutepe <barisk@gmail.com>
2020-08-15 03:44:13 +0200

    change mq to pull for harsh network conditions

--- mq.py
+++ mq.py
@@ -2,9 +2,9 @@
 import time
 import signal
 from threading import Event, Lock
-from google.api_core.exceptions import AlreadyExists
+from google.api_core.exceptions import AlreadyExists, DeadlineExceeded
 from google.cloud import pubsub_v1
-from google.cloud.pubsub_v1.proto.pubsub_pb2 import PubsubMessage
+from google.cloud.pubsub_v1.proto.pubsub_pb2 import PubsubMessage, ReceivedMessage
 from podcomm.pdm import Pdm
 from podcomm.pod import Pod
 from podcomm.definitions import *
@@ -45,6 +45,20 @@ def get_json(message_data):
     return json.loads(bytes.decode(message_data, encoding='UTF-8'))
 
 
+def ntp_update():
+    os.system('sudo systemctl stop ntp')
+    os.system('sudo ntpd -gq')
+    os.system('sudo systemctl start ntp')
+
+
+def restart():
+    os.system('shutdown -r now')
+
+
+def shutdown():
+    os.system('shutdown -h now')
+
+
 class MqOperator(object):
     def __init__(self):
         configureLogging()
@@ -55,26 +69,16 @@ class MqOperator(object):
         with open("settings.json", "r") as stream:
             self.settings = json.load(stream)
 
-        self.i_pdm = None
-        self.i_pod = None
-        self.g_pdm = None
-        self.g_pod = None
+        self.i_pdm: Pdm = None
+        self.i_pod: Pod = None
+        self.g_pdm: Pdm = None
+        self.g_pod: Pod = None
 
-        self.decimal_zero = Decimal("0")
-        self.i_rate_requested = None
-        self.i_rate_duration_requested = None
-        self.i_bolus_requested = self.decimal_zero
-        self.g_rate_requested = None
-        self.g_bolus_requested = self.decimal_zero
-
-        self.started = time.time()
         self.insulin_bolus_pulse_interval = 4
         self.next_pdm_run = time.time()
 
-        self.message_event = Event()
         self.exit_event = Event()
         self.stop_event = Event()
-        self.requests_lock = Lock()
         self.requests = []
         self.executed_req_ids = []
 
@@ -123,17 +127,11 @@ class MqOperator(object):
             self.i_pdm = Pdm(self.i_pod)
             self.i_pdm.start_radio()
 
-            self.subscription_future = self.subscriber.subscribe(self.subscription_path,
-                                                                 callback=self.subscription_callback,
-                                                                 # scheduler=ThreadScheduler(
-                                                                 #     executor=ThreadPoolExecutor(max_workers=2))
-                                                                 )
             while True:
-                self.message_event.wait(10)
+                self.pull_messages()
                 if self.exit_event.is_set():
                     break
-                if self.message_event.is_set():
-                    self.process_requests()
+                self.process_requests()
 
         except Exception:
             raise
@@ -142,47 +140,54 @@ class MqOperator(object):
             self.publisher.stop()
             self.stop_event.set()
 
-    def subscription_callback(self, message: PubsubMessage):
+    def pull_messages(self):
+        while True:
+            try:
+                response = self.subscriber.pull(self.subscription_path, max_messages=5, timeout=3)
+            except DeadlineExceeded:
+                break
+
+            if self.exit_event.is_set():
+                break
+            for received_msg in response.received_messages:
+                self.process_message(received_msg)
+                if self.exit_event.is_set():
+                    break
+
+    def process_message(self, received_msg: ReceivedMessage):
+        message = received_msg.message
         try:
             new_request = get_json(message.data)
-            new_request['message'] = message
+            new_request['req_msg_id'] = message.message_id
+            new_request['req_msg_ack_id'] = received_msg.ack_id
+            new_request['req_msg_publish_time'] = message.publish_time.seconds * 1000 + message.publish_time.nanos
 
-            with self.requests_lock:
-                if new_request['id'] in self.executed_req_ids:
-                    message.ack()
-                else:
-                    for request in self.requests:
-                        if request['message'].message_id == message.message_id\
-                                or request['id'] == new_request['id']:
-                            message.ack()
-                            break
-                    else:
-                        self.logger.debug(f'new request received, type: {new_request["type"]}, id: {new_request["id"]}')
-                        self.requests.append(new_request)
-                        self.message_event.set()
+            if new_request['id'] in self.executed_req_ids\
+                    or new_request['req_msg_id'] in [r['req_msg_id'] for r in self.requests]:
+                self.ack(received_msg.ack_id)
+            else:
+                self.logger.debug(f'new request received, type: {new_request["type"]}, id: {new_request["id"]}')
+                self.requests.append(new_request)
+
         except Exception as e:
             self.logger.error("Error parsing message in callback", e)
-            message.nack()
+            self.nack(received_msg.ack_id)
 
     def process_requests(self):
         while True:
-            req = None
-            with self.requests_lock:
-                self.message_event.clear()
-                self.filter_expired()
-                self.filter_outdated()
-                self.filter_redundant()
-                self.sort_requests()
+            self.filter_expired()
+            self.filter_outdated()
+            self.filter_redundant()
+            self.sort_requests()
 
-                if len(self.requests) > 0:
-                    req = self.requests[0]
-                    self.requests = self.requests[1:]
-                    self.executed_req_ids.append(req['id'])
-
-            if req is None:
+            if len(self.requests) > 0:
+                req = self.requests[0]
+                self.requests = self.requests[1:]
+                self.executed_req_ids.append(req['id'])
+            else:
                 break
-            msg = req['message']
-            msg.ack()
+
+            self.ack(req["req_msg_ack_id"])
             try:
                 self.perform_request(req)
                 self.send_response(req, "success")
@@ -193,13 +198,12 @@ class MqOperator(object):
     def filter_expired(self):
         not_expired = []
         for req in self.requests:
-            msg = req['message']
             if "expiration" in req and req["expiration"] is not None:
                 expiration = int(req["expiration"])
             else:
-                expiration = int(msg.publish_time.timestamp() * 1000) + 60 * 1000
+                expiration = int(req["req_msg_publish_time"]) + 90 * 1000
             if get_now() > expiration:
-                msg.ack()
+                self.ack(req["req_msg_ack_id"])
                 self.send_response(req, "expired")
             else:
                 not_expired.append(req)
@@ -209,11 +213,10 @@ class MqOperator(object):
         up_to_date = []
         pod = self.i_pod.__dict__
         for req in self.requests:
-            msg = req['message']
             if "state" in req and req["state"] is not None:
                 last_state = int(pod["state_last_updated"] * 1000)
                 if req["state"] != last_state:
-                    msg.ack()
+                    self.ack(req["req_msg_ack_id"])
                     self.send_response(req, "outdated")
                     continue
 
@@ -224,12 +227,12 @@ class MqOperator(object):
         non_redundant = []
         types = {}
 
-        self.requests.sort(key=lambda r: r['message'].publish_time.timestamp(), reverse=True)
+        self.requests.sort(key=lambda r: r['req_msg_publish_time'],
+                           reverse=True)
         for req in self.requests:
-            msg = req['message']
             req_type = req["type"]
             if req_type in types:
-                msg.ack()
+                self.ack(req["req_msg_ack_id"])
                 self.send_response(req, "redundant")
                 continue
 
@@ -247,7 +250,9 @@ class MqOperator(object):
     def send_response(self, request: dict, result: str):
         self.logger.debug(f'responding to request {request["id"]}: {result}')
         request_copy = request.copy()
-        request_copy.pop('message')
+        request_copy.pop('req_msg_id')
+        request_copy.pop('req_msg_ack_id')
+        request_copy.pop('req_msg_publish_time')
         response = {
             'request': request_copy,
             'result': result,
@@ -274,13 +279,27 @@ class MqOperator(object):
             basal_rate = ticks_to_decimal(int(rp["ticks"]))
             basal_duration = seconds_to_hours(int(rp["duration"]))
             self.i_pdm.set_temp_basal(basal_rate, basal_duration)
+        elif req_type == "deactivate":
+            self.i_pdm.deactivate_pod()
+        elif req_type == "update_time":
+            ntp_update()
+        elif req_type == "restart":
+            restart()
+        elif req_type == "shutdown":
+            shutdown()
         else:
             raise InvalidOperation
 
+    def ack(self, ack_id):
+        self.subscriber.acknowledge(self.subscription_path, [ack_id])
 
-def _exit_with_grace(operator: MqOperator):
-    operator.exit_event.set()
-    operator.stop_event.wait()
+    def nack(self, ack_id):
+        self.subscriber.modify_ack_deadline(self.subscription_path, [ack_id], 0)
+
+
+def _exit_with_grace(mqo: MqOperator):
+    mqo.exit_event.set()
+    mqo.stop_event.wait()
 
 
 if __name__ == '__main__':
